308979e7f2c307a4edf1b926069f9300fd9822be
Fedor Semochkin <221800ss@gmail.com>
2026-02-03 14:16:27 +0700

    update cask

--- Casks/codebase-parser.rb
+++ Casks/codebase-parser.rb
@@ -1,10 +1,17 @@
 cask "codebase-parser" do
   version "1.1.0"
 
-  arch arm: "aarch64", intel: "x64"
-  url "https://github.com/Fedorse/Codebase-parser/releases/download/v#{version}/Parser-#{version}-macOS-#{arch}.dmg"  sha256 arm:   "3924c36d216f95266d4cd80a7d72977de103069beadac8c4662a18138a4324e4",
-         intel: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  if Hardware::CPU.intel?
 
+    arch = "x64"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  else
+
+    arch = "aarch64"
+    sha256 "3924c36d216f95266d4cd80a7d72977de103069beadac8c4662a18138a4324e4"
+  end
+
+  url "https://github.com/Fedorse/Codebase-parser/releases/download/v#{version}/Parser-#{version}-macOS-#{arch}.dmg"
   name "Codebase Parser"
   desc "Desktop codebase parser & analyzer"
   homepage "https://github.com/Fedorse/Codebase-parser"
