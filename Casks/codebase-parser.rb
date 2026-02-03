b04a88e6ed254ee7a3ac3a4e0cd91dd3260d74a1
Fedor Semochkin <221800ss@gmail.com>
2026-02-03 13:57:52 +0700

    update cask

--- Casks/codebase-parser.rb
+++ Casks/codebase-parser.rb
@@ -1,17 +1,15 @@
 cask "codebase-parser" do
-  version "0.1.0"
-  # 1. Get this hash by running: shasum -a 256 /path/to/your/downloaded.dmg
-  sha256 "REPLACE_WITH_ACTUAL_SHA256_HASH_OF_THE_DMG"
+  version "1.1.0"
 
-  # 2. This URL points to your GitHub Release artifact
-  url "https://github.com/Fedorse/Codebase-parser/releases/download/v#{version}/codebase-parser_#{version}_aarch64.dmg"
+  arch arm: "aarch64", intel: "x64"
+  url "https://github.com/Fedorse/Codebase-parser/releases/download/v#{version}/Parser-#{version}-macOS-#{arch}.dmg"  sha256 arm:   "3924c36d216f95266d4cd80a7d72977de103069beadac8c4662a18138a4324e4",
+         intel: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   name "Codebase Parser"
   desc "Desktop codebase parser & analyzer"
   homepage "https://github.com/Fedorse/Codebase-parser"
 
-  # 3. This matches the name of the app inside the dmg
-  app "Codebase Parser.app"
+  app "codebase-parser.app"
 
   zap trash: [
     "~/Library/Application Support/com.parser.app",
@@ -19,4 +17,3 @@ cask "codebase-parser" do
     "~/Library/Saved Application State/com.parser.app.savedState",
   ]
 end
-
